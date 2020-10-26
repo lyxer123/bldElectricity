6ef16ba79440c792f4f8114c5f834ef765174559
huangzhi <1049729282@qq.com>
2020-10-26 18:17:49 +0800

    2020.10.26提交修改页面

--- src/views/traceability/traceability.vue
+++ src/views/traceability/traceability.vue
@@ -1,12 +1,41 @@
 <!--  -->
 <template>
-<div class=''>
+<div id="app">
     <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="nodeName" label="节点名称" />
-        <el-table-column prop="nodeType" label="节点类型" />
-        <el-table-column prop="nodeLatency" label="节点延时时间" />
-        <el-table-column prop="lastBlock" label="区块高度" />
+        <el-table-column prop="nodeName" label="node name" />
+        <el-table-column prop="nodeType" label="node type" />
+        <el-table-column prop="nodeLatency" label="node latency" />
+        <el-table-column prop="nodeMining" label="node mining" />
+        <el-table-column prop="nodePeers" label="node peers" />
+        <el-table-column prop="lastBlock" label="last block" />
+        <el-table-column prop="totalDifficulty" label="total difficulty" />
+        <el-table-column prop="blockTransactions" label="block transactions" />
+        <el-table-column prop="blockUncles" label="block uncles" />
+        <el-table-column prop="lastBlockTime" label="last block time" />
+        <el-table-column prop="propagationTime" label="propagation time" />
+
     </el-table>
+    <div>
+        <h2 style="font-size: small">LAST BLOCKS MINERS</h2>
+        <h3 style="font-size: small">{{tableData[0].lastBlockMiners1}}</h3>
+        <h3 style="font-size: small">{{tableData[0].lastBlockMiners2}}</h3>
+
+    </div>
+    <el-row :gutter="20">
+        <el-col :span="6">
+            <div class="grid-content bg-purple">#{{tableData[0].lastBlock.toLocaleString()}}</div>
+        </el-col>
+        <el-col :span="6">
+            <div class="grid-content bg-purple">{{tableData[0].lastBlockTime}}</div>
+        </el-col>
+        <el-col :span="6">
+            <div class="grid-content bg-purple">{{tableData[0].avgBlocktime}}</div>
+        </el-col>
+        <el-col :span="6">
+            <div class="grid-content bg-purple">{{tableData[0].avgHashrate}}</div>
+        </el-col>
+    </el-row>
+
 </div>
 </template>
 
@@ -15,6 +44,7 @@
 //例如：import 《组件名称》 from '《组件路径》';
 let websocket;
 export default {
+    name: 'app',
     //import引入的组件需要注入到对象中才能使用
     components: {},
     data() {
@@ -24,8 +54,20 @@ export default {
                 nodeName: '',
                 nodeType: '',
                 nodeLatency: '',
+                nodeMining: '',
+                nodePeers: '',
                 lastBlock: '',
-            }]
+                totalDifficulty: '',
+                blockTransactions: '0',
+                blockUncles: '0',
+                lastBlockTime: '',
+                propagationTime: '',
+                lastBlockMiners1: '',
+                lastBlockMiners2: '',
+                avgBlocktime: '',
+                avgHashrate: '',
+            }],
+
         };
     },
     //监听属性 类似于data概念
@@ -35,8 +77,10 @@ export default {
     //方法集合
     methods: {
         getData() {
+            setInterval(() => {
+                this.tableData[0].lastBlockTime = parseInt(this.tableData[0].lastBlockTime) + 1 + "s ago";
+            }, 1000);
             websocket = new WebSocket("ws://125.64.98.21:3000/primus/?_primuscb=" + new Date().getTime() + "-1");
-            // websocket = new WebSocket("ws://125.64.98.21:6006");
             websocket.onopen = () => {
                 websocket.send(JSON.stringify({
                     "emit": ["ready"]
@@ -44,26 +88,44 @@ export default {
             };
 
             websocket.onmessage = e => {
-                let data = JSON.parse(e.data);
-                let emit = data.emit;
-                console.log(data);
+                let domData = JSON.parse(e.data);
+                let emit = domData.emit;
+                console.log(domData);
                 if (emit && emit[0] === "init") {
                     this.tableData[0].nodeName = emit[1].nodes[0].info.name;
                     this.tableData[0].nodeType = emit[1].nodes[0].info.node;
-                    this.tableData[0].nodeLatency = emit[1].nodes[0].info.net;
+                    this.tableData[0].nodeLatency = emit[1].nodes[0].stats.latency;
+                    this.tableData[0].nodeMining = emit[1].nodes[0].stats.mining == true ? 0 : 1;
+                    this.tableData[0].nodePeers = emit[1].nodes[0].stats.peers;
                     this.tableData[0].lastBlock = emit[1].nodes[0].stats.block.number;
+                    this.tableData[0].lastBlockTime = Math.round((new Date().getTime() / 1000 - emit[1].nodes[0].stats.block.timestamp)) + "s ago";
+
                 } else {
-                    if (data.action === "block") {
-                        this.tableData[0].lastBlock = data.data.block.number;
+                    if (domData.action === "block") {
+                        this.tableData[0].lastBlock = domData.data.block.number;
+                        this.tableData[0].totalDifficulty = domData.data.block.totalDifficulty;
+                        this.tableData[0].blockTransactions = domData.data.block.transactions.length;
+                        this.tableData[0].blockUncles = domData.data.block.uncles.length;
+                        this.tableData[0].lastBlockTime = "0s ago";
+                        this.tableData[0].propagationTime = domData.data.block.propagation + " ms";
+                    } else if (domData.action === "charts") {
+                        this.$nextTick(() => {
+                            this.tableData[0].lastBlockMiners1 = domData.data.miners[0].miner;
+                            this.tableData[0].lastBlockMiners2 = domData.data.miners[1].miner;
+                            this.tableData[0].avgBlocktime = domData.data.avgBlocktime.toFixed(2) + "s";
+                            this.tableData[0].avgHashrate = domData.data.avgHashrate.toFixed(1) + "H/s";
+
+                        })
+
                     }
                 }
-
             }
         },
+
     },
     //生命周期 - 创建完成（可以访问当前this实例）
     created() {
-        this.getData()
+        this.getData();
     },
     //生命周期 - 挂载完成（可以访问DOM元素）
     mounted() {
@@ -79,4 +141,48 @@ export default {
 
     }, //如果页面有keep-alive缓存功能，这个函数会触发
 }
+
+// 基于准备好的dom，初始化echarts实例
+
+// 指定图表的配置项和数据
+
+// 使用刚指定的配置项和数据显示图表。
 </script>
+
+<style>
+.el-row {
+    margin-bottom: 20px;
+
+    &:last-child {
+        margin-bottom: 0;
+    }
+}
+
+.el-col {
+    border-radius: 4px;
+}
+
+.bg-purple-dark {
+    background: #99a9bf;
+}
+
+.bg-purple {
+    background: black;
+}
+
+.bg-purple-light {
+    background: #e5e9f2;
+}
+
+.grid-content {
+    border-radius: 4px;
+    min-height: 90px;
+    color: blue;
+    text-align: center;
+}
+
+.row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+}
+</style>
